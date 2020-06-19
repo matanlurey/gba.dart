4a54216b977dbb677d0117b70b3afa915ae00039
Matan Lurey <matan@lurey.org>
2020-06-19 16:29:16 -0700

    Start a simpler assembler.

--- lib/src/assembler/thumb.dart
+++ lib/src/assembler/thumb.dart
@@ -1,262 +1,118 @@
 import 'package:binary/binary.dart';
-import 'package:meta/meta.dart';
 
-/// Represents a decoded `THUMB` instruction based on `ARMv4T` architecture.
-abstract class ThumbInstruction {
-  /// Returns a [ThumbInstruction] by decoding a 16-bit [encoded] value.
-  ///
-  /// Throws a [FormatException] if unrecognized.
-  factory ThumbInstruction.decode(Uint16 encoded) {
-    ThumbInstruction decoded;
-    for (final format in ThumbDecoder.formats) {
-      decoded = format.decode(encoded);
-      if (decoded != null) {
-        return decoded;
+enum ThumbBinaryOpcodeFormat {
+  // ---------------------------------------------------------------------------
+  // Register Opterations (ALU, BLX)
+  // ---------------------------------------------------------------------------
+
+  /// Move Shifted Register.
+  moveShiftedRegister,
+
+  /// Add or Subtract.
+  addOrSubtract,
+
+  /// Move, Compare, Add, Subtract Immediate.
+  moveOrCompareOrAddOrSubtractImmediate,
+
+  /// ALU Operations.
+  aluOperations,
+
+  /// Hi Register Operations or Branch Exchange.
+  hiRegisterOperationsOrBranchExchange,
+
+  // ---------------------------------------------------------------------------
+  // Memory Load/Store (LDR/STR)
+  // ---------------------------------------------------------------------------
+
+  /// Load PC-Relative (for loading immediates from the literal pool).
+  loadPcRelative,
+
+  /// Load or Store with Register Offset.
+  loadOrStoreWithRegisterOffset,
+
+  /// Load or Store Sign-Extended Byte/Half-Word.
+  loadOrStoreSignExtendedByteOrHalfWord,
+
+  /// Load or Store with Immediate Offset.
+  loadOrStoreWithImmediateOffset,
+
+  /// Load or Store Half-Word.
+  loadOrStoreHalfWord,
+
+  /// Load or Store SP-Relative.
+  loadOrStoreSpRelative,
+
+  // ---------------------------------------------------------------------------
+  // Memory Addressing (ADD PC/SP)
+  // ---------------------------------------------------------------------------
+
+  /// Get Relative Address.
+  getRelativeAddress,
+
+  /// Add Offset to Stack Pointer.
+  addOffsetToStackPointer,
+
+  // ---------------------------------------------------------------------------
+  // Memory Multiple Load/Store (PUSH/POP and LDM/STM)
+  // ---------------------------------------------------------------------------
+
+  /// Push or Pop Registers.
+  pushOrPopRegisters,
+
+  /// Multiple Load or Store.
+  multipleLoadOrStore,
+
+  // ---------------------------------------------------------------------------
+  // Jumps and Calls
+  // ---------------------------------------------------------------------------
+
+  /// Conditional Branch.
+  conditionalBranch,
+
+  /// Unconditional Branch.
+  unconditionalBranch,
+
+  /// Long Branch with Link.
+  longBranchWithLink,
+
+  /// Software Interrupt and Breakpoint.
+  softwareInterruptAndBreakpoint,
+}
+
+extension ThumbBinaryOpcodeFormatX on ThumbBinaryOpcodeFormat {
+  /// Matches a [ThumbBinaryOpcodeFormat] type given an [encoded] instruction.
+  static ThumbBinaryOpcodeFormat match(Uint32 encoded) {
+    for (var i = ThumbBinaryOpcodeFormat.values.length - 1; i >= 0; i--) {
+      final format = ThumbBinaryOpcodeFormat.values[i];
+      if (encoded.value & format.mask == format.mask) {
+        return format;
       }
     }
-    throw FormatException('Not a valid ARMv4T THUMB instruction', encoded);
+    throw StateError('No match found: ${encoded.toBinaryPadded()}');
   }
-}
 
-/// Decodes byte-encoded instructions into [ThumbInstruction] instances.
-///
-/// See http://infocenter.arm.com/help/topic/com.arm.doc.ddi0210c/graphics/thumb_instruction_set_formats.svg.
-@visibleForTesting
-abstract class ThumbDecoder {
-  /// Known THUMB-formats that require different decoders.
-  static final formats = <ThumbDecoder>[
-    MoveShiftedRegister(),
-    AddAndSubtract(),
-    MoveCompareAddAndSubtractImmediate(),
-    ALUOperation(),
-    HighRegisterOperationsAndBranchExchange(),
-    PCRelativeLoad(),
-    LoadAndStoreWithRelativeOffset(),
-    LoadAndStoreHalfword(),
-    LoadAndStoreWithImmediateOffset(),
-    SPRelativeLoadAndStore(),
-    LoadAddress(),
-    AddOffsetToStackPointer(),
-    PushAndPopRegisters(),
-    MultipleLoadAndStore(),
-    ConditionalBranch(),
-    SoftwareInterrupt(),
-    UnconditionalBranch(),
-    LongBranchWithLink(),
+  static final _masks = [
+    ('0000' '0000' '0000' '0000'.parseBits()), // 01: Move Shifted Register.
+    ('0001' '1100' '0000' '0000'.parseBits()), // 02: Add And Subtract.
+    ('0010' '0000' '0000' '0000'.parseBits()), // 03: Move, Compare, Add, ...
+    ('0100' '0000' '0000' '0000'.parseBits()), // 04: ALU Operation.
+    ('0100' '0100' '0000' '0000'.parseBits()), // 05: High Register ...
+    ('0100' '1000' '0000' '0000'.parseBits()), // 06: PC-Relative Load.
+    ('0101' '0000' '0000' '0000'.parseBits()), // 07: Load And Store With ...
+    ('0101' '0010' '0000' '0000'.parseBits()), // 08: Load And Store Sign-...
+    ('0110' '0000' '0000' '0000'.parseBits()), // 09: Load And Store With ...
+    ('1000' '0000' '0000' '0000'.parseBits()), // 10: Load And Store Half-World.
+    ('1001' '0000' '0000' '0000'.parseBits()), // 11: SP-Relative Load And ...
+    ('1010' '0000' '0000' '0000'.parseBits()), // 12: Load Address.
+    ('1011' '0000' '0000' '0000'.parseBits()), // 13: Add Offset To Stack ...
+    ('1011' '0100' '0000' '0000'.parseBits()), // 14: Push And Pop Registers.
+    ('1100' '0000' '0000' '0000'.parseBits()), // 15: Multiple Load And Store.
+    ('1101' '0000' '0000' '0000'.parseBits()), // 16: Conditional Branch.
+    ('1101' '1111' '0000' '0000'.parseBits()), // 17: Software Interrupt.
+    ('1110' '0000' '0000' '0000'.parseBits()), // 18: Unconditional Branch.
+    ('1111' '0000' '0000' '0000'.parseBits()), // 19: Long Branch With Link.
   ];
 
-  /// Name of the instruction format.
-  final String name;
-
-  /// Format type integer.
-  final int value;
-
-  /// A mask value used to determine if decoded can be processed for this type.
-  final int mask;
-
-  const ThumbDecoder._(this.value, this.name, this.mask)
-      : assert(name != null),
-        assert(value != null),
-        assert(mask != null);
-
-  @nonVirtual
-  ThumbInstruction decode(Uint16 encoded) {
-    return shouldDecode(encoded) ? _decode(encoded) : null;
-  }
-
-  @nonVirtual
-  bool shouldDecode(Uint16 encoded) => encoded.value & mask == mask;
-
-  /// Implementation of [decode].
-  ThumbInstruction _decode(Uint16 encoded) => throw UnimplementedError();
-}
-
-@visibleForTesting
-class MoveShiftedRegister extends ThumbDecoder {
-  MoveShiftedRegister()
-      : super._(
-          01,
-          'Move shifted register',
-          '111'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class AddAndSubtract extends ThumbDecoder {
-  AddAndSubtract()
-      : super._(
-          02,
-          'Add and subtract',
-          '000111'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class MoveCompareAddAndSubtractImmediate extends ThumbDecoder {
-  MoveCompareAddAndSubtractImmediate()
-      : super._(
-          03,
-          'Move, compare, add, and subtract immediate',
-          '001'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class ALUOperation extends ThumbDecoder {
-  ALUOperation()
-      : super._(
-          04,
-          'ALU operation',
-          '01'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class HighRegisterOperationsAndBranchExchange extends ThumbDecoder {
-  HighRegisterOperationsAndBranchExchange()
-      : super._(
-          05,
-          'High register operations and branch exchange',
-          '010001'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class PCRelativeLoad extends ThumbDecoder {
-  PCRelativeLoad()
-      : super._(
-          06,
-          'PC-relative load',
-          '01001'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class LoadAndStoreWithRelativeOffset extends ThumbDecoder {
-  LoadAndStoreWithRelativeOffset()
-      : super._(
-          07,
-          'Load and store with relative offset',
-          '0101'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class LoadAndStoreSignExtendedByteAndHalfword extends ThumbDecoder {
-  LoadAndStoreSignExtendedByteAndHalfword()
-      : super._(
-          08,
-          'Load and store sign-extended byte and halfword',
-          '0101001'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class LoadAndStoreWithImmediateOffset extends ThumbDecoder {
-  LoadAndStoreWithImmediateOffset()
-      : super._(
-          09,
-          'Load and store with immediate offset',
-          '011'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class LoadAndStoreHalfword extends ThumbDecoder {
-  LoadAndStoreHalfword()
-      : super._(
-          10,
-          'Load and store halfword',
-          '1'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class SPRelativeLoadAndStore extends ThumbDecoder {
-  SPRelativeLoadAndStore()
-      : super._(
-          11,
-          'SP-relative load and store',
-          '1001'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class LoadAddress extends ThumbDecoder {
-  LoadAddress()
-      : super._(
-          12,
-          'Load address',
-          '101'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class AddOffsetToStackPointer extends ThumbDecoder {
-  AddOffsetToStackPointer()
-      : super._(
-          13,
-          'Add offset to stack pointer',
-          '1011'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class PushAndPopRegisters extends ThumbDecoder {
-  PushAndPopRegisters()
-      : super._(
-          14,
-          'Push and pop registers',
-          '101101'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class MultipleLoadAndStore extends ThumbDecoder {
-  MultipleLoadAndStore()
-      : super._(
-          15,
-          'Multiple load and store',
-          '1100'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class ConditionalBranch extends ThumbDecoder {
-  ConditionalBranch()
-      : super._(
-          16,
-          'Conditional branch',
-          '1101'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class SoftwareInterrupt extends ThumbDecoder {
-  SoftwareInterrupt()
-      : super._(
-          17,
-          'Software interrupt',
-          '11011111'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class UnconditionalBranch extends ThumbDecoder {
-  UnconditionalBranch()
-      : super._(
-          18,
-          'Unconditional branch',
-          '111'.padRight(16, '0').parseBits(),
-        );
-}
-
-@visibleForTesting
-class LongBranchWithLink extends ThumbDecoder {
-  LongBranchWithLink()
-      : super._(
-          19,
-          'Long branch with link',
-          '1111'.padRight(16, '0').parseBits(),
-        );
+  /// Mask value to match this format in an encoded instruction.
+  int get mask => _masks[index];
 }
--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,3 +1,5 @@
+import 'package:binary/binary.dart';
+
 /// Whether `assert` function is executed in the current environment.
 ///
 /// By default this will often be `true` in development and tests, and `false`
@@ -12,4 +14,19 @@ bool get assertionsEnabled {
   var enabled = false;
   assert(enabled = true);
   return enabled;
+}
+
+extension IntX on int {
+  /// Returns [intValue], asserting that it is the same as [checkBits].
+  int check([String check]) {
+    if (assertionsEnabled && check != null) {
+      assert(
+        this.toBinaryPadded(check.length) == check,
+        ''
+        'Expected 0x${int.parse(check, radix: 2).toRadixString(16)}, '
+        'got 0x${this.toRadixString(16)}',
+      );
+    }
+    return this;
+  }
 }
